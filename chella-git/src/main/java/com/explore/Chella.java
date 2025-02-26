cd198530b7a31ec5dbb002e2cfe529347b4b752a
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2025-02-26 16:16:47 +0100

    update: add method ✅

--- chella-git/src/main/java/com/explore/Chella.java
+++ chella-git/src/main/java/com/explore/Chella.java
@@ -2,9 +2,12 @@ package com.explore;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Chella {
     private static final String REPO_DIR = ".chella/";
+    private static final String OBJECTS_DIR = "objects/";
 
     private static void init() throws IOException {
         Path repoPath = Paths.get(REPO_DIR);
@@ -13,14 +16,45 @@ public class Chella {
             return;
         }
         Files.createDirectories(repoPath);
-        Files.createDirectories(Paths.get(REPO_DIR + "objects"));
+        Files.createDirectories(Paths.get(REPO_DIR + OBJECTS_DIR));
         Files.createFile(Paths.get(REPO_DIR + "HEAD"));
         Files.createFile(Paths.get(REPO_DIR + "index"));
         System.out.println("Initializing empty chella repository.");
     }
 
-    private static void add(String fileName){
+    private static String hashContent(byte[] content){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hash = digest.digest(content);
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                hexString.append(String.format("%02X", b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No such algorithm.");
+            return null;
+        }
+    }
 
+    private static void add(String fileName){
+        try {
+            byte[] fileContent = Files.readAllBytes(Path.of(fileName));
+            String hash = hashContent(fileContent);
+            if (hash != null) {
+                String stagingFolder = hash.substring(0,2) + "/";
+                Path subFolderPath = Paths.get(OBJECTS_DIR, stagingFolder);
+                Files.createDirectories(subFolderPath);
+
+                String stagingFileName = hash.substring(2);
+                Path filePath = Paths.get(subFolderPath.toString(), stagingFileName);
+                Files.write(filePath, fileContent);
+            } else {
+                System.out.println("No such algorithm.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -31,6 +65,13 @@ public class Chella {
             switch (command) {
                 case "init":
                     init();
+                    break;
+                case "add":
+                    if (args.length < 2) {
+                        System.out.println("Usage: java Chella add <file>");
+                        return;
+                    }
+                    add(args[1]);
                     break;
             }
         }  catch (IOException e) {
